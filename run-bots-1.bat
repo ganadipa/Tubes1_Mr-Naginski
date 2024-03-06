0323423f2432c3c61e62dc4107d7e1c3aa2f7444
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-03-06 21:58:21 +0700

    feat: tester utils

---
+++ run-bots-1.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python main.py --logic Putih --email=test34@email.com --name=putihh --password=123456 --team etimo"
+start cmd /c "python main.py --logic Mahal --email=test13@email.com --name=kodokmahal --password=123456 --team etimo"
+start cmd /c "python main.py --logic Putih --email=test34_2@email.com --name=putihh2 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Mahal --email=test13_2@email.com --name=kodokmahl2 --password=123456 --team etimo"
--- run-bots-3.bat
+++ run-bots-3.bat
@@ -1,5 +1,5 @@
 @echo off
 start cmd /c "python main.py --logic VietCong --email=test42@email.com --name=vietcongg --password=123456 --team etimo"
-start cmd /c "python main.py --logic Putih --email=test34@email.com --name=putihh --password=123456 --team etimo"
+start cmd /c "python main.py --logic Mahal --email=test13@email.com --name=kodokmahal --password=123456 --team etimo"
 start cmd /c "python main.py --logic VietCong --email=test42_2@email.com --name=vietcongg2 --password=123456 --team etimo"
-start cmd /c "python main.py --logic Putih --email=test34_2@email.com --name=putihh2 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Mahal --email=test13_2@email.com --name=kodokmahl2 --password=123456 --team etimo"
